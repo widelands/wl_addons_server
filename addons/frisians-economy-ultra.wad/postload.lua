0a0d5260dc4c492080712b8fc5cd299b00447da1
Benedikt Straub <benedikt-straub@web.de>
2021-05-03 12:39:44 +0200

    Add some more translators comments

--- addons/frisians-economy-ultra.wad/postload.lua
+++ addons/frisians-economy-ultra.wad/postload.lua
@@ -100,14 +100,18 @@ descriptions:modify_unit("tribe", "frisians", "add_building", "frisians_rye_farm
 --------------------------------------------------------------------------------
 
 descriptions:modify_unit("productionsite", "frisians_smokery", "input", "add_ware", "salt", 6)
-descriptions:modify_unit("productionsite", "frisians_smokery", "programs", "set", "smoke_fish", { descname = _"smoking fish", actions = {
+descriptions:modify_unit("productionsite", "frisians_smokery", "programs", "set", "smoke_fish", {
+      -- TRANSLATORS: Completed/Skipped/Did not start smoking fish because ...
+      descname = _"smoking fish", actions = {
             "return=skipped unless economy needs smoked_fish",
             "consume=fish:2 salt log",
             "sleep=duration:16s",
             "animate=working duration:30s",
             "produce=smoked_fish:2"
       }})
-descriptions:modify_unit("productionsite", "frisians_smokery", "programs", "set", "smoke_meat", { descname = _"smoking meat", actions = {
+descriptions:modify_unit("productionsite", "frisians_smokery", "programs", "set", "smoke_meat", {
+      -- TRANSLATORS: Completed/Skipped/Did not start smoking meat because ...
+      descname = _"smoking meat", actions = {
             "return=skipped when site has fish:2 and economy needs smoked_fish",
             "return=skipped unless economy needs smoked_meat",
             "consume=meat:2 salt log",
@@ -115,7 +119,9 @@ descriptions:modify_unit("productionsite", "frisians_smokery", "programs", "set"
             "animate=working duration:30s",
             "produce=smoked_meat:2"
       }})
-descriptions:modify_unit("productionsite", "frisians_smokery", "programs", "set", "main", { descname = _"working", actions = {
+descriptions:modify_unit("productionsite", "frisians_smokery", "programs", "set", "main", {
+      -- TRANSLATORS: Completed/Skipped/Did not start working because ...
+      descname = _"working", actions = {
             "call=smoke_fish",
             "call=smoke_meat"
       }})
@@ -124,7 +130,9 @@ descriptions:modify_unit("productionsite", "frisians_bakery", "input", "remove_w
 descriptions:modify_unit("productionsite", "frisians_bakery", "input", "modify_ware", "water", 5)
 descriptions:modify_unit("productionsite", "frisians_bakery", "input", "add_ware", "barley_flour", 5)
 descriptions:modify_unit("productionsite", "frisians_bakery", "input", "add_ware", "rye_flour", 5)
-descriptions:modify_unit("productionsite", "frisians_bakery", "programs", "set", "main", { descname = _"baking bread", actions = {
+descriptions:modify_unit("productionsite", "frisians_bakery", "programs", "set", "main", {
+      -- TRANSLATORS: Completed/Skipped/Did not start baking bread because ...
+      descname = _"baking bread", actions = {
             "return=skipped unless economy needs bread_frisians or workers need experience",
             "consume=water barley_flour rye_flour",
             "sleep=duration:20s",
@@ -137,21 +145,27 @@ descriptions:modify_unit("productionsite", "frisians_honey_bread_bakery", "input
 descriptions:modify_unit("productionsite", "frisians_honey_bread_bakery", "input", "modify_ware", "honey", 4)
 descriptions:modify_unit("productionsite", "frisians_honey_bread_bakery", "input", "add_ware", "barley_flour", 5)
 descriptions:modify_unit("productionsite", "frisians_honey_bread_bakery", "input", "add_ware", "rye_flour", 5)
-descriptions:modify_unit("productionsite", "frisians_honey_bread_bakery", "programs", "set", "bake_honey", { descname = _"baking honey bread", actions = {
+descriptions:modify_unit("productionsite", "frisians_honey_bread_bakery", "programs", "set", "bake_honey", {
+      -- TRANSLATORS: Completed/Skipped/Did not start baking honey bread because ...
+      descname = _"baking honey bread", actions = {
             "return=skipped unless economy needs honey_bread or workers need experience",
             "consume=water barley_flour rye_flour honey",
             "sleep=duration:20s",
             "animate=working duration:25s",
             "produce=honey_bread"
       }})
-descriptions:modify_unit("productionsite", "frisians_honey_bread_bakery", "programs", "set", "bake_normal", { descname = _"baking bread", actions = {
+descriptions:modify_unit("productionsite", "frisians_honey_bread_bakery", "programs", "set", "bake_normal", {
+      -- TRANSLATORS: Completed/Skipped/Did not start baking bread because ...
+      descname = _"baking bread", actions = {
             "return=skipped unless economy needs bread_frisians or workers need experience",
             "consume=water barley_flour rye_flour",
             "sleep=duration:20s",
             "animate=working duration:20s",
             "produce=bread_frisians"
       }})
-descriptions:modify_unit("productionsite", "frisians_honey_bread_bakery", "programs", "set", "main", { descname = _"working", actions = {
+descriptions:modify_unit("productionsite", "frisians_honey_bread_bakery", "programs", "set", "main", {
+      -- TRANSLATORS: Completed/Skipped/Did not start working because ...
+      descname = _"working", actions = {
             "call=bake_honey",
             "call=bake_normal"
       }})
