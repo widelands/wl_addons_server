2596942923658ae84daca19f676fb1860c142814
Noordfrees <Noordfrees@users.noreply.github.com>
2021-08-08 11:24:15 +0000

    Your code was automatically formatted.

--- munin/Munin.java
+++ munin/Munin.java
@@ -45,32 +45,46 @@ public class Munin {
 		return new SubGraph(cmd, descname, "Counter of " + descname + " commands", "COUNTER");
 	}
 	private static final MultiGraph[] _graphs = new MultiGraph[] {
-		new MultiGraph("addons_uptime", "Add-Ons Server Uptime", "time",
-			new SubGraph("time", "Uptime in ms", "Time in milliseconds since the last server restart", "GAUGE")
-		),
-		new MultiGraph("addons_users", "Add-Ons Server Users", "count",
-			new SubGraph("registered", "Registered", "Number of currently connected registered users", "GAUGE"),
-			new SubGraph("unregistered", "Unregistered", "Number of currently connected unregistered users", "GAUGE"),
-			new SubGraph("unique", "Unique", "Counter of unique registered users", "COUNTER"),
-			new SubGraph("failed", "Failed", "Counter of failed connection attempts", "COUNTER")
-		),
-		new MultiGraph("addons_commands", "Add-Ons Commands", "count",
-			// Keep the order in sync with the server's wl.server.Command constant ordering
-			cmdGraph("list"),
-			cmdGraph("info"),
-			cmdGraph("download"),
-			cmdGraph("i18n"),
-			cmdGraph("screenshot"),
-			cmdGraph("vote"),
-			cmdGraph("get_vote"),
-			cmdGraph("comment"),
-			cmdGraph("edit_comment"),
-			cmdGraph("submit"),
-			cmdGraph("submit_screenshot"),
-			cmdGraph("contact"),
-			cmdGraph("setup_tx"),
-			new SubGraph("failed", "Failed", "Counter of failed commands", "COUNTER")
-		),
+	    new MultiGraph("addons_uptime",
+	                   "Add-Ons Server Uptime",
+	                   "time",
+	                   new SubGraph("time",
+	                                "Uptime in ms",
+	                                "Time in milliseconds since the last server restart",
+	                                "GAUGE")),
+	    new MultiGraph(
+	        "addons_users",
+	        "Add-Ons Server Users",
+	        "count",
+	        new SubGraph("registered",
+	                     "Registered",
+	                     "Number of currently connected registered users",
+	                     "GAUGE"),
+	        new SubGraph("unregistered",
+	                     "Unregistered",
+	                     "Number of currently connected unregistered users",
+	                     "GAUGE"),
+	        new SubGraph("unique", "Unique", "Counter of unique registered users", "COUNTER"),
+	        new SubGraph("failed", "Failed", "Counter of failed connection attempts", "COUNTER")),
+	    new MultiGraph(
+	        "addons_commands",
+	        "Add-Ons Commands",
+	        "count",
+	        // Keep the order in sync with the server's wl.server.Command constant ordering
+	        cmdGraph("list"),
+	        cmdGraph("info"),
+	        cmdGraph("download"),
+	        cmdGraph("i18n"),
+	        cmdGraph("screenshot"),
+	        cmdGraph("vote"),
+	        cmdGraph("get_vote"),
+	        cmdGraph("comment"),
+	        cmdGraph("edit_comment"),
+	        cmdGraph("submit"),
+	        cmdGraph("submit_screenshot"),
+	        cmdGraph("contact"),
+	        cmdGraph("setup_tx"),
+	        new SubGraph("failed", "Failed", "Counter of failed commands", "COUNTER")),
 	};
 	private static final String HOST = "127.0.0.1";
 	private static final String PASSWORD = "Odin";
@@ -80,21 +94,21 @@ public class Munin {
 		if (args.length > 0 && args[0].equals("config")) {
 			for (MultiGraph g : _graphs) {
 				System.out.println("multigraph " + g.name);
-    			System.out.println("graph_title " + g.descname);
-    			System.out.println("graph_args -l 0");
-    			System.out.println("graph_vlabel " + g.axisLabel);
-    			System.out.println("graph_category widelands");
+				System.out.println("graph_title " + g.descname);
+				System.out.println("graph_args -l 0");
+				System.out.println("graph_vlabel " + g.axisLabel);
+				System.out.println("graph_category widelands");
 
-    			System.out.print("graph_order");
-    			for (SubGraph s : g.subgraphs) System.out.print(" " + s.name);
-    			System.out.println();
+				System.out.print("graph_order");
+				for (SubGraph s : g.subgraphs) System.out.print(" " + s.name);
+				System.out.println();
 
-    			for (SubGraph s : g.subgraphs) {
-    				System.out.println(s.name + ".label " + s.shortInfo);
-    				System.out.println(s.name + ".info " + s.longInfo);
-    				System.out.println(s.name + ".type " + s.type);
-    				System.out.println(s.name + ".draw LINE1");
-    			}
+				for (SubGraph s : g.subgraphs) {
+					System.out.println(s.name + ".label " + s.shortInfo);
+					System.out.println(s.name + ".info " + s.longInfo);
+					System.out.println(s.name + ".type " + s.type);
+					System.out.println(s.name + ".draw LINE1");
+				}
 			}
 			return;
 		}
@@ -121,17 +135,19 @@ public class Munin {
 			md5.println(r);
 			md5.close();
 			out.println(new BufferedReader(new InputStreamReader(p.getInputStream()))
-				    .readLine()
-				    .split(" ")[0]);
+			                .readLine()
+			                .split(" ")[0]);
 			out.println("ENDOFSTREAM");
 
-			for (String[] s : output) for (int i = 0; i < s.length; i++) s[i] = in.readLine();
+			for (String[] s : output)
+				for (int i = 0; i < s.length; i++) s[i] = in.readLine();
 		} catch (Exception e) {
 			System.err.println(e);
 		} finally {
 			for (int i = 0; i < output.length; i++) {
 				System.out.println("multigraph " + _graphs[i].name);
-				for (int j = 0; j < output[i].length; j++) System.out.println(_graphs[i].subgraphs[j].name + ".value " + output[i][j]);
+				for (int j = 0; j < output[i].length; j++)
+					System.out.println(_graphs[i].subgraphs[j].name + ".value " + output[i][j]);
 			}
 		}
 	}
