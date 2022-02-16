e53cc59ec5c7987588969ade583e44cdfb652522
The Widelands Bunnybot <bunnybot@widelands.org>
2022-02-16 03:05:01 +0100

    Automated server sync

--- addons/europeans_tribe.wad/workers/miner_advanced/init.lua
+++ addons/europeans_tribe.wad/workers/miner_advanced/init.lua
@@ -10,6 +10,8 @@ descriptions:new_worker_type {
     icon = dirname .. "menu.png",
     vision_range = 2,
     
+    buildcost = { europeans_stonecutter_advanced = 1 },
+    
     animations = {
         idle = {
             hotspot = { 7, 22 }
