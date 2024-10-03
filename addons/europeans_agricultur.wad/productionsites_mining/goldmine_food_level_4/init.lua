a261db208dad162fcf69f904e2c87ab1b0c8b632
The Widelands Bunnybot <bunnybot@widelands.org>
2024-10-03 03:10:40 +0200

    Automated server sync

--- addons/europeans_agricultur.wad/productionsites_mining/goldmine_food_level_4/init.lua
+++ addons/europeans_agricultur.wad/productionsites_mining/goldmine_food_level_4/init.lua
@@ -5,7 +5,7 @@ dirname = path.dirname(__file__)
 wl.Descriptions():new_productionsite_type {
     name = "europeans_goldmine_level_4",
     -- TRANSLATORS: This is a building name used in lists of buildings
-    descname = pgettext("europeans_building", "Gold Mine Level 4 (Food)"),
+    descname = pgettext("europeans_building", "Gold Mine Level 4"),
     icon = dirname .. "menu.png",
     
     animation_directory = dirname,
@@ -52,8 +52,8 @@ wl.Descriptions():new_productionsite_type {
     },
 
     inputs = {
-        { name = "honey_bread", amount = 4 },
         { name = "mead", amount = 4 },
+        { name = "honey_bread", amount = 2 },
         { name = "meat", amount = 2 },
         { name = "fish", amount = 2 }
     },
@@ -63,8 +63,9 @@ wl.Descriptions():new_productionsite_type {
             -- TRANSLATORS: Completed/Skipped/Did not start mining gold because ...
             descname = pgettext("europeans_building", "mining"),
             actions = {
-                "return=skipped unless economy needs ore or economy needs granite or economy needs quartz or workers need experience",
-                "return=skipped when economy needs coal and not economy needs ore",
+                "return=skipped when economy needs mead and not economy needs granite and not economy needs ore and not economy needs quartz",
+                "return=skipped when economy needs honey_bread and not economy needs granite and not economy needs ore and not economy needs quartz",
+                "return=skipped when economy needs coal and not economy needs ore and not economy needs quartz",
                 "consume=honey_bread",
                 "call=mine_stone",
                 "call=mine_ore",
@@ -128,7 +129,7 @@ wl.Descriptions():new_productionsite_type {
             -- just a dummy program to fix encyclopedia
             descname = "encyclopedia",
             actions = {
-                "consume=honey_bread mead:2 meat,fish",
+                "consume=mead:2 honey_bread meat,fish",
                 "produce=ore:10 granite:4 marble:2 quartz:2",
             }
         },
