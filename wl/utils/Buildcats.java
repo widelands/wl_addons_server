6dce89ceccf8f8621df22809cf1464eb113b9a93
Benedikt Straub <benedikt-straub@web.de>
2026-06-10 12:10:54 +0200

    Handle multiline config strings in xgettext

--- wl/utils/Buildcats.java
+++ wl/utils/Buildcats.java
@@ -21,10 +21,12 @@ package wl.utils;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
@@ -46,6 +48,68 @@ public class Buildcats {
 		    filename.equals("dirnames") || filename.equals("descriptions");
 	}
 
+	private static void doRunXgettext(String file, String out) throws Exception {
+		doRunXgettext(file, new FileInputStream(file), out);
+	}
+
+	private static void doRunXgettext(String filename, InputStream fileContent, String out)
+	    throws Exception {
+		final boolean needsProcessing = !filename.toLowerCase().endsWith("lua");
+		Utils.Profile profile = null;
+		if (needsProcessing) profile = Utils.readProfile(fileContent, filename);
+
+		ProcessBuilder pb = new ProcessBuilder(
+		    new String[] {"xgettext",
+		                  "-k_",
+		                  "--keyword=_",
+		                  "--flag=_:1:pass-lua-format",
+		                  "--keyword=ngettext:1,2",
+		                  "--flag=ngettext:1:pass-lua-format",
+		                  "--flag=ngettext:2:pass-lua-format",
+		                  "--keyword=pgettext:1c,2",
+		                  "--flag=pgettext:2:pass-lua-format",
+		                  "--keyword=npgettext:1c,2,3",
+		                  "--flag=npgettext:2:pass-lua-format",
+		                  "--flag=npgettext:3:pass-lua-format",
+		                  "--language=Lua",
+		                  "--from-code=UTF-8",
+		                  "-F",
+		                  "-c TRANSLATORS:",
+		                  "--join-existing",
+		                  "--force-po",
+		                  "--copyright-holder=\"Widelands Development Team\"",
+		                  "--msgid-bugs-address=\"https://www.widelands.org/wiki/ReportingBugs/\"",
+		                  "--output=" + out,
+		                  "-"});
+		pb.redirectErrorStream(true);
+		Process p = pb.start();
+		OutputStream pipe = p.getOutputStream();
+		PrintStream print = needsProcessing ? new PrintStream(pipe) : null;
+
+		if (needsProcessing) {
+			profile.write(print);
+		} else {
+			for (;;) {
+				int i = fileContent.read();
+				if (i < 0) break;
+				pipe.write(i);
+			}
+		}
+
+		if (print != null) {
+			print.flush();
+			print.close();
+		} else {
+			pipe.flush();
+			pipe.close();
+		}
+		fileContent.close();
+		BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String str;
+		while ((str = b.readLine()) != null) Utils.log("    # " + str);
+		p.waitFor();
+	}
+
 	private static void recurse(String out, File dir, boolean mapsOnly) throws Exception {
 		Utils.log("Buildcats: Recursing in " + dir.getPath());
 		for (File f : Utils.listSorted(dir)) {
@@ -56,41 +120,10 @@ public class Buildcats {
 				for (Enumeration e = zip.entries(); e.hasMoreElements();) {
 					ZipEntry entry = (ZipEntry)e.nextElement();
 					if (entry.isDirectory() || !isXGettextInput(entry.getName())) continue;
-					InputStream input = zip.getInputStream(entry);
-					ProcessBuilder pb = new ProcessBuilder(new String[] {
-					    "xgettext", "-k_", "--keyword=_", "--flag=_:1:pass-lua-format",
-					    "--keyword=ngettext:1,2", "--flag=ngettext:1:pass-lua-format",
-					    "--flag=ngettext:2:pass-lua-format", "--keyword=pgettext:1c,2",
-					    "--flag=pgettext:2:pass-lua-format", "--keyword=npgettext:1c,2,3",
-					    "--flag=npgettext:2:pass-lua-format", "--flag=npgettext:3:pass-lua-format",
-					    "--language=Lua", "--from-code=UTF-8", "-F",
-					    "-c TRANSLATORS:", "--join-existing", "--output=" + out, "-"});
-					pb.redirectErrorStream(true);
-					Process p = pb.start();
-					OutputStream pipe = p.getOutputStream();
-					for (;;) {
-						int i = input.read();
-						if (i < 0) break;
-						pipe.write(i);
-					}
-					pipe.flush();
-					pipe.close();
-					input.close();
-					BufferedReader b =
-					    new BufferedReader(new InputStreamReader(p.getInputStream()));
-					String str;
-					while ((str = b.readLine()) != null) Utils.log("    # " + str);
-					p.waitFor();
+					doRunXgettext(entry.getName(), zip.getInputStream(entry), out);
 				}
 			} else if (!mapsOnly && isXGettextInput(f.getName())) {
-				Utils.bash("xgettext", "-k_", "--keyword=_", "--flag=_:1:pass-lua-format",
-				           "--keyword=ngettext:1,2", "--flag=ngettext:1:pass-lua-format",
-				           "--flag=ngettext:2:pass-lua-format", "--keyword=pgettext:1c,2",
-				           "--flag=pgettext:2:pass-lua-format", "--keyword=npgettext:1c,2,3",
-				           "--flag=npgettext:2:pass-lua-format",
-				           "--flag=npgettext:3:pass-lua-format", "--language=Lua",
-				           "--from-code=UTF-8", "-F", "-c TRANSLATORS:", "--join-existing",
-				           "--output=" + out, f.getPath());
+				doRunXgettext(f.getPath(), out);
 			}
 		}
 	}
@@ -130,19 +163,13 @@ public class Buildcats {
 
 		recurse(out, new File(Utils.config("website_maps_path")), true);
 
-		Utils.bash("xgettext", "--language=Lua", "-k_", "--from-code=UTF-8", "--output=" + out,
-		           "--force-po", "--copyright-holder=\"Widelands Development Team\"",
-		           "--msgid-bugs-address=\"https://www.widelands.org/wiki/ReportingBugs/\"",
-		           "--join-existing", uploaderCommentsFile.getAbsolutePath());
+		doRunXgettext(uploaderCommentsFile.getAbsolutePath(), out);
 
 		for (File addon : Utils.listSorted(new File("addons"))) {
 			dir = new File("po", addon.getName());
 			dir.mkdir();
 			out = dir.getPath() + "/" + addon.getName() + ".pot";
-			Utils.bash("xgettext", "--language=Lua", "-k_", "--from-code=UTF-8", "--output=" + out,
-			           "--force-po", "--copyright-holder=\"Widelands Development Team\"",
-			           "--msgid-bugs-address=\"https://www.widelands.org/wiki/ReportingBugs/\"",
-			           addon.getPath() + "/addon");
+			doRunXgettext(addon.getPath() + "/addon", out);
 			recurse(out, new File("screenshots", addon.getName()), false);
 			recurse(out, addon, false);
 		}
